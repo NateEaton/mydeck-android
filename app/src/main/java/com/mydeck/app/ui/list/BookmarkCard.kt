26f78d2c1fbd0a0989fd26cdb962b8ba301e2667
Nate Eaton <nate.eaton.jr@gmail.com>
2026-01-31 22:02:04 -0600

    Fix missing import for Luminance

--- app/src/main/java/com/mydeck/app/ui/list/BookmarkCard.kt
+++ app/src/main/java/com/mydeck/app/ui/list/BookmarkCard.kt
@@ -47,6 +47,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.luminance
 import coil3.ColorImage
 import coil3.compose.AsyncImage
 import coil3.compose.AsyncImagePreviewHandler
